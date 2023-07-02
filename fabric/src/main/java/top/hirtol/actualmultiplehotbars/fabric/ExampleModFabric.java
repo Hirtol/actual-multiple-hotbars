3aa5ef609ecbd46517b58bd866212e7eb89b0df6
Valentijn Hol <git@hirtol.top>
2023-07-02 22:36:04 +0200

    Initial commit working

---
+++ fabric/src/main/java/top/hirtol/actualmultiplehotbars/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package top.hirtol.actualmultiplehotbars.fabric;
+
+import top.hirtol.actualmultiplehotbars.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
---
+++ forge/src/main/java/top/hirtol/actualmultiplehotbars/forge/ExampleExpectPlatformImpl.java
@@ -0,0 +1,15 @@
+package top.hirtol.actualmultiplehotbars.forge;
+
+import top.hirtol.actualmultiplehotbars.ExampleExpectPlatform;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+
+public class ExampleExpectPlatformImpl {
+    /**
+     * This is our actual method to {@link ExampleExpectPlatform#getConfigDirectory()}.
+     */
+    public static Path getConfigDirectory() {
+        return FMLPaths.CONFIGDIR.get();
+    }
+}

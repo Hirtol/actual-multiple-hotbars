f0314211a03c8b3075675065cf4953883d972026
Valentijn Hol <git@hirtol.top>
2023-10-07 16:21:48 +0200

    Record historic inventory state in case the player wants to restore it after the fact.

--- common/src/main/java/top/hirtol/actualmultiplehotbars/inventory/ServerInventoryManager.java
+++ common/src/main/java/top/hirtol/actualmultiplehotbars/inventory/ServerInventoryManager.java
@@ -15,19 +15,20 @@ import top.hirtol.actualmultiplehotbars.ActualHotbars;
 public class ServerInventoryManager extends PersistentState {
 
   public HashMap<UUID, HotbarInventory> players = new HashMap<>();
+  /**
+   * Stores the state of the hotbar inventory at the time of the most recent death.
+   */
+  public HashMap<UUID, NbtCompound> historicInventories = new HashMap<>();
 
   @Override
   public NbtCompound writeNbt(NbtCompound nbt) {
     // Putting the 'players' hashmap, into the 'nbt' which will be saved.
-    NbtCompound playersNbtCompound = new NbtCompound();
-    players.forEach((UUID, playerState) -> {
-      NbtCompound playerStateNbt = new NbtCompound();
-
-      playerState.writeNbt(playerStateNbt);
-
-      playersNbtCompound.put(String.valueOf(UUID), playerStateNbt);
-    });
+    NbtCompound playersNbtCompound = createHashNbt(players);
     nbt.put("players", playersNbtCompound);
+
+    NbtCompound historicNbtCompound = new NbtCompound();
+    historicInventories.forEach(((uuid, nbtCompound) -> historicNbtCompound.put(uuid.toString(), nbtCompound)));
+    nbt.put("historicInventories", historicNbtCompound);
 
     return nbt;
   }
@@ -44,14 +45,16 @@ public class ServerInventoryManager extends PersistentState {
   public static ServerInventoryManager createFromNbt(NbtCompound tag) {
     ServerInventoryManager serverState = new ServerInventoryManager();
 
-    NbtCompound playersTag = tag.getCompound("players");
-    playersTag.getKeys().forEach(key -> {
-      HotbarInventory playerState = new HotbarInventory();
-      playerState.readNbt(playersTag.getCompound(key));
+    if (tag.contains("historicInventories")) {
+      NbtCompound historicTag = tag.getCompound("historicInventories");
+      historicTag.getKeys().forEach(key -> {
+        UUID uuid = UUID.fromString(key);
+        serverState.historicInventories.put(uuid, tag.getCompound(key));
+      });
+    }
 
-      UUID uuid = UUID.fromString(key);
-      serverState.players.put(uuid, playerState);
-    });
+    NbtCompound playersTag = tag.getCompound("players");
+    serverState.players = readFromNbt(playersTag);
 
     return serverState;
   }
@@ -75,5 +78,44 @@ public class ServerInventoryManager extends PersistentState {
     } else {
       return null;
     }
+  }
+
+  public static void persistHistoricInventory(PlayerEntity player) {
+    if (player.world.getServer() != null) {
+      ServerInventoryManager state = getManager(player.world.getServer());
+      HotbarInventory partial = state.players.computeIfAbsent(player.getUuid(), uuid -> new HotbarInventory());
+      NbtCompound playerStateNbt = new NbtCompound();
+      partial.writeNbt(playerStateNbt);
+
+      state.historicInventories.put(player.getUuid(), playerStateNbt);
+    }
+  }
+
+  private static NbtCompound createHashNbt(HashMap<UUID, HotbarInventory> state) {
+    NbtCompound compound = new NbtCompound();
+
+    state.forEach((UUID, playerState) -> {
+      NbtCompound playerStateNbt = new NbtCompound();
+
+      playerState.writeNbt(playerStateNbt);
+
+      compound.put(String.valueOf(UUID), playerStateNbt);
+    });
+
+    return compound;
+  }
+
+  private static HashMap<UUID, HotbarInventory> readFromNbt(NbtCompound tag) {
+    HashMap<UUID, HotbarInventory> state = new HashMap<>();
+
+    tag.getKeys().forEach(key -> {
+      HotbarInventory playerState = new HotbarInventory();
+      playerState.readNbt(tag.getCompound(key));
+
+      UUID uuid = UUID.fromString(key);
+      state.put(uuid, playerState);
+    });
+
+    return state;
   }
 }
